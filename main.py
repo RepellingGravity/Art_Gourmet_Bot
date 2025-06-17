c0c32a3215e563668a0010346960670c5bd3f36a
RepellingGravity <danleone42@gmail.com>
2025-06-17 19:52:26 -0400

    fixing consistency in each function using wait for instead of expect

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ URP_MENU_4_FILEPATH = "URP_4.txt"
 
 DAYS_OF_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 MEAL_TIMES = ["Breakfast", "Lunch", "Dinner"]
+ACCOUNT_NAMES = ["URP_North", "URP_South", "Aria_WPB", "Aria_Boca", "Arcstone", "Mandala", "Legacy", "Ignite", "Archway"]
 
 def main():
     with open(URP_MENU_4_FILEPATH, 'r') as menu:
@@ -20,36 +21,22 @@ def main():
     account_name = lines[0]
     menu_number = int(lines[1][-1])
     
-    playwright, browser, page = start_playwright()
     username, password = input("username and password separated by space: ").split(" ")
-    login(page, username, password)
+    
+    playwright, browser, page = start_playwright()
+    jolt_login(page, username, password)
     go_to_lists_homepage(page)
-    set_group_mode(page)
-    set_group_root_folder(page)
-    select_folder(page, DRIVER_CHECKLISTS_FOLDER_NAME)
-    
-    add_list(page, "test name")
-    
-    add_list_item(page)
-    page.wait_for_timeout(250)
-    select_checkmark(page)
-    page.wait_for_timeout(250)
-    select_list_item(page, 0)
-    page.wait_for_timeout(250)
+    set_group_mode(page, JOLT_GROUP_NAME)
+    set_group_root_folder(page, JOLT_GROUP_NAME)
+    click_on_text(page, DRIVER_CHECKLISTS_FOLDER_NAME)
+    click_add_list(page, "test name")
+    click_add_list_item(page)
+    click_checkmark_item(page)
     fill_prompt_text(page, "checkmark test fill")
-    page.wait_for_timeout(250)
-    
-    add_list_item(page)
-    page.wait_for_timeout(250)
-    select_photo(page)
-    page.wait_for_timeout(250)
-    select_list_item(page, 1)
-    page.wait_for_timeout(250)
+    click_add_list_item(page)
+    click_photo_item(page)
     fill_prompt_text(page, "photo test fill")
-    page.wait_for_timeout(250)
-    
     save_list(page)
-    
     time.sleep(10000)
     stop_playwright(browser, playwright)
     
@@ -63,50 +50,66 @@ def stop_playwright(browser, playwright):
     browser.close()
     playwright.stop()
     
-def login(page, username, password):
+def jolt_login(page, username, password):
     page.goto(JOLT_LOGIN_URL)
     page.get_by_role("textbox", name="email").fill(username)
     page.get_by_role("textbox", name="password").fill(password)
     page.get_by_role("button", name="Submit login").click()
-    expect(page.get_by_role("heading", name="Lists Overview")).to_be_visible()
+    page.get_by_role("heading", name="Lists Overview").wait_for(state="visible")
     
 def go_to_lists_homepage(page):
     page.goto(JOLT_LISTS_URL)
+    page.get_by_role("table").locator(".list-template-index-table").wait_for(state="visible")
+    # must use user facing elements table didnt work but "Name" in the column did since we can 
+    # directly see it, try in all places to get to the front most part of the tree
 
-def set_group_mode(page):
-    page.locator('//*[@id="header"]/div[1]/div[2]/button').click() # top right dropdown menu
-    page.wait_for_timeout(1000)
-    page.locator('//*[@id="php-menu-export"]/div/div/div/div/div[1]/div/div[2]/div/button[1]').click() # menu item
-    page.wait_for_timeout(1000)
+def set_group_mode(page, group_name):
+    page.locator(".entity-button isEntitySwitcherButton").click()
+    page.locator(".entity-tiles").wait_for(state="visible")
+    page.get_by_role("button").get_by_text(group_name).click()
+    page.locator(".list-template-header").wait_for(state="hidden")
+    page.locator(".list-template-header").wait_for(state="visible")
 
-def set_group_root_folder(page):
-    page.locator('//*[@id="root"]/div[1]/div/div[1]/div[1]/div[2]').click() # group root folder
+def set_group_root_folder(page, folder_name):
+    page.locator(".name body2").get_by_text(folder_name).click()
     
-def select_folder(page, name):
-    page.get_by_text(DRIVER_CHECKLISTS_FOLDER_NAME).click()
+def click_on_text(page, text):
+    page.get_by_text(text).click()
     
-def add_list(page, name):
+def click_add_list(page, list_name):
     page.get_by_test_id("button-circle").click()
-    page.get_by_test_id("input-list-titlefield-input-undefined").fill(name)
+    page.get_by_role("textbox", name="list-title").fill(list_name)
     page.get_by_role("button").get_by_text("Confirm").click()
+    page.get_by_role("heading", name="Edit List Name").wait_for(state="hidden")
     
-def add_list_item(page):
-    page.get_by_test_id("button-layout-addItem").click()
+def click_add_list_item(page):
+    page.get_by_role("button").get_by_text("+ ADD ITEM").click()
+    page.get_by_role("header", name="Select an item type").wait_for(state="visible")
     
-def select_checkmark(page):
-    page.get_by_test_id("undefined").get_by_text("Checkmark").first.click()
+def click_checkmark_item(page):
+    page.locator(".list-check-bold").click()
+    page.locator(".list-check-bold").wait_for(state="hidden")
+    verify_list_item_active(page)
     
-def select_photo(page):
-    page.get_by_test_id("undefined").get_by_text("Photo").first.click()
+def click_photo_item(page):
+    page.locator(".list-camera").click()
+    page.locator(".list-camera").wait_for(state="hidden")
+    verify_list_item_active(page)
     
-def select_list_item(page, item_index):
-    page.locator("#itemTemplate-newItem-" + str(item_index + 1)).click()
-
+def verify_list_item_active(page):
+    item_index = page.locator(".itemTemplateCardDragDrop").count() - 1
+    page.locator(".itemTemplateCardDragDrop").nth(item_index).locator("is-selected").wait_for(state="visible")
+    
 def fill_prompt_text(page, text):
-    page.get_by_test_id("textArea-field-textarea-undefined").fill(text)
+    page.get_by_role("textbox", name="text").fill(text)
     
 def save_list(page):
-    page.get_by_test_id("button-header-save").click()
-    
+    page.get_by_role("button").get_by_text("SAVE").click()
+    page.get_by_role("heading", name="Edit List Name").wait_for(state="visible")
+    page.get_by_role("button").get_by_text("Cancel").click()
+    page.get_by_role("heading", name="Edit List Name").wait_for(state="hidden")
+    page.get_by_role("button", name="Previous Page Button").click()
+    page.get_by_role("table").locator(".list-template-index-table").wait_for(state="visible")
+
 if __name__ == "__main__":
     main()
